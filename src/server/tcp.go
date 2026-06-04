a455691834e57f6a65661423ce5c0323dbad2129
Utpal Sen <contactutpalsen@gmail.com>
2026-06-04 19:57:42 +0530

    feat: implement ECHO parsing logic
    Signed-off-by: Utpal Sen <contactutpalsen@gmail.com>

--- src/server/tcp.go
+++ src/server/tcp.go
@@ -33,7 +33,33 @@ func handleConn(conn net.Conn) {
 	defer conn.Close()
 
 	buf := make([]byte, 1024)
-	conn.Read(buf)
 
-	conn.Write([]byte("+PONG\r\n"))
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+		return
+	}
+	_, parsedResp := resp.ReadNextRESP(buf[:n])
+
+	var args []string
+
+	if parsedResp.Type == resp.Array {
+		parsedResp.ForEach(func(r resp.RESP) bool {
+			args = append(args, r.String())
+			return true
+		})
+	}
+
+	if len(args) > 0 {
+		command := strings.ToUpper(args[0])
+
+		if command == "PING" {
+			conn.Write([]byte("+PONG\r\n"))
+		} else if command == "ECHO" && len(args) > 1 {
+			wordToEcho := args[1]
+
+			formattedResponse := resp.AppendBulkString(nil, wordToEcho)
+			conn.Write(formattedResponse)
+		}
+	}
 }
